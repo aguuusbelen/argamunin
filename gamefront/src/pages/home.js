53b079be1c787f004eec4eebbce854d50dbbf60d
Agus <agustinabmendez@gmail.com>
2024-02-01 23:34:06 -0300

    formating code

--- gamefront/src/pages/home.js
+++ gamefront/src/pages/home.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "../styles/page.module.scss";
 
 export default function Home() {
   return (
@@ -7,7 +7,7 @@ export default function Home() {
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          <code className={styles.code}>app/page.js</code>
         </p>
         <div>
           <a
