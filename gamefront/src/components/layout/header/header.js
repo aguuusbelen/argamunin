cfbf33ca3d8aadb846f375e50adaf6d0c783e5b0
Agus <agustinabmendez@gmail.com>
2024-02-04 17:09:59 -0300

    adding quantico fonts

--- gamefront/src/components/layout/header/header.js
+++ gamefront/src/components/layout/header/header.js
@@ -1,10 +1,5 @@
 import styles from './header.module.scss';
 
 export default function Header() {
-  return (
-    <div className={styles.header}>
-      LOGO - Nombre Empresa - Tabs
-    </div>
-  );
+  return <div className={styles.header}>LOGO - Nombre Empresa - Tabs </div>;
 }
-  
