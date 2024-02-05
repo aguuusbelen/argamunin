37e749ed99dabce115617b3430469beeba277e50
Agus <agustinabmendez@gmail.com>
2024-02-04 21:25:09 -0300

    update styles layout

--- gamefront/src/components/layout/footer/footer.js
+++ gamefront/src/components/layout/footer/footer.js
@@ -2,6 +2,14 @@ import styles from './footer.module.scss';
 
 export default function Footer() {
   console.log(styles);
-  return <div className={styles.footer}>cosas - links y otras cosas - Nombre Empresa - Tabs</div>;
+  return (
+    <div className={styles.footer}>
+      <div className={styles.containerTabs}>
+        <button className={styles.buttonTab}>Novedades</button>
+        <button className={styles.buttonTab}>Trabaja con nosotros</button>
+        <button className={styles.buttonTab}>Biblioteca de juegos</button>
+        <button className={styles.buttonTab}>Politicas de privacidad</button>
+      </div>
+    </div>
+  );
 }
-  
---
+++ gamefront/src/pages/home.js
@@ -0,0 +1,12 @@
+import styles from '../styles/home.module.scss';
+
+export default function Home() {
+  return (
+    <div>
+      <div className={styles.container}>
+        This is home and hust has more text
+        <p className="h4">ESTE TEXTO va a ser gris </p>
+      </div>
+    </div>
+  );
+}
