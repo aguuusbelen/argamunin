43c6a8d88736882d7a50c422b76f60b2556654ac
lautarodebenedetti <lautarodebenedetti@gmail.com>
2024-02-08 21:39:58 -0300

    config mongodb

--- backend/src/main/java/argamunin/com/backend/BackendApplication.java
+++ backend/src/main/java/argamunin/com/backend/BackendApplication.java
@@ -1,8 +1,18 @@
 package argamunin.com.backend;
 
+import org.bson.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @SpringBootApplication
@@ -10,5 +20,26 @@ public class BackendApplication {
     
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+		
+		// MONGO DB CONEXION
+		String connectionString = "mongodb+srv://argamunin:rCD573GNOysZxpEY@argamunin.eieratd.mongodb.net/?retryWrites=true&w=majority";
+	    ServerApi serverApi = ServerApi.builder()
+	            .version(ServerApiVersion.V1)
+	            .build();
+	    MongoClientSettings settings = MongoClientSettings.builder()
+	            .applyConnectionString(new ConnectionString(connectionString))
+	            .serverApi(serverApi)
+	            .build();
+	    // Create a new client and connect to the server
+	    try (MongoClient mongoClient = MongoClients.create(settings)) {
+	        try {
+	            // Send a ping to confirm a successful connection
+	            MongoDatabase database = mongoClient.getDatabase("admin");
+	            database.runCommand(new Document("ping", 1));
+	            System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+	        } catch (MongoException e) {
+	            e.printStackTrace();
+	        }
+	    }
 	}
 }
