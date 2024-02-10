79df948ee65cb316f9ca5d496fd4452cfdaa3375
lautarodebenedetti <lautarodebenedetti@gmail.com>
2024-02-10 16:48:33 -0300

    adding game entity

--- backend/src/main/java/argamunin/com/backend/entitys/Game.java
+++ backend/src/main/java/argamunin/com/backend/entitys/Game.java
@@ -1,6 +1,41 @@
 package argamunin.com.backend.entitys;
 
+import java.awt.Image;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "games")
 public class Game {
+	@Id
     private int id;
     private String name;
+    private List<Image> images;
+    private String description;
+    
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Image> getImages() {
+		return images;
+	}
+	public void setImages(List<Image> images) {
+		this.images = images;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
