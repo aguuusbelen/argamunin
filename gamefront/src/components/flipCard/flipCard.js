3757433fac1a0558c83791dc2adb4f834d42e55e
Agus <agustinabmendez@gmail.com>
2024-07-02 22:44:43 -0300

    fix error

--- gamefront/src/components/flipCard/flipCard.js
+++ gamefront/src/components/flipCard/flipCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const FlipCard = ({ frontContent, backContent }) => {
   const [isFlipped, setIsFlipped] = useState(false);
